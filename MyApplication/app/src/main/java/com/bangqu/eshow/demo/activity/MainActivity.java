152d1ce2ed4da4024bd3da4fb21e761c6a84e0e4
daikting <775715400@qq.com>
2016-01-26 15:37:03 +0800

    eshow基础测试demo源码

---
+++ MyApplication/app/src/main/java/com/bangqu/eshow/demo/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bangqu.eshow.demo.activity;
+
+
+import android.os.Bundle;
+
+import com.bangqu.eshow.demo.R;
+
+public class MainActivity extends CommonActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
